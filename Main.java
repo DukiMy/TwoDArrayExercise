f9aac629d7d0cbd511320b777230f55da10a06b9
Durim Miziraj <durim.miziraj@gmail.com>
2023-04-26 15:13:30 +0200

    Cleaned the main method.

--- Main.java
+++ Main.java
@@ -6,49 +6,64 @@
  */
 public class Main {    
 
+    // Declaring variables
+    static String firstRow;
+    static String secondRow;
+    static String thirdRow;
+    static String pos00 = "   ";
+    static String pos01 = "   ";
+    static String pos02 = "   ";
+    static String pos10 = "   ";
+    static String pos11 = "   ";
+    static String pos12 = "   ";
+    static String pos20 = "   ";
+    static String pos21 = "   ";
+    static String pos22 = "   ";
+    
+    // Setting the final strings for output.
+    static final String POS_MARKER = "███";
+    static final String TABLE_TITLE = "          Item table";
+    static final String COLUMN_INDEX_POINTERS = "           i-0            i-1        i-2";
+    static final String COLUMN_SPECIFIERS = "          | ITEM NUMBER | QUANTITY | PRICE |";
+    static final String HORIZONTAL_LINE = "          ----------------------------------";
+
+    // Declaring a 2-dimensional array.
+    static final String[][] ITEM_TABLE = new String[][] {
+        {pos00, pos01, pos02},
+        {pos10, pos11, pos12},
+        {pos20, pos21, pos22}
+    };
+
     public static void main(String[] args) {
-
-        // Declaring variables
-        String pos00 = "   ";
-        String pos01 = "   ";
-        String pos02 = "   ";
-        String pos10 = "   ";
-        String pos11 = "   ";
-        String pos12 = "   ";
-        String pos20 = "   ";
-        String pos21 = "   ";
-        String pos22 = "   ";
-        final String POS_MARKER = "███";
         
-        // Declaring a 2-dimensional array.
-        String[][] ITEM_TABLE = new String[][] {
-            {pos00, pos01, pos02},
-            {pos10, pos11, pos12},
-            {pos20, pos21, pos22}
-        };
-
         // CHANGE VALUES HERE!!! CHANGE VALUES HERE!!! CHANGE VALUES HERE!!! 
         ITEM_TABLE[1][2] = POS_MARKER;    // Anything other than number 0, 1 and 2 will crash the program.
 
-        // Setting the final strings for output.
-        final  String TABLE_TITLE = "          Item table";
-        final  String COLUMN_INDEX_POINTERS = "           i-0            i-1        i-2";
-        final  String COLUMN_SPECIFIERS = "          | ITEM NUMBER | QUANTITY | PRICE |";
-        final  String HORIZONTAL_LINE = "          ----------------------------------";
-        final  String FIRST_ROW = String.format("i-0       | %s         | %s      | %s   |", ITEM_TABLE[0][0], ITEM_TABLE[0][1], ITEM_TABLE[0][2]);
-        final  String SECOND_ROW = String.format("i-0       | %s         | %s      | %s   |", ITEM_TABLE[1][0], ITEM_TABLE[1][1], ITEM_TABLE[1][2]);
-        final  String THIRD_ROW = String.format("i-0       | %s         | %s      | %s   |", ITEM_TABLE[2][0], ITEM_TABLE[2][1], ITEM_TABLE[2][2]);
+        setRowValues();
+        printTable();
 
-        // Prints out the table.
+    }
+
+    private static void setRowValues() {
+
+        firstRow = String.format("i-0       | %s         | %s      | %s   |", ITEM_TABLE[0][0], ITEM_TABLE[0][1], ITEM_TABLE[0][2]);
+        secondRow = String.format("i-0       | %s         | %s      | %s   |", ITEM_TABLE[1][0], ITEM_TABLE[1][1], ITEM_TABLE[1][2]);
+        thirdRow = String.format("i-0       | %s         | %s      | %s   |", ITEM_TABLE[2][0], ITEM_TABLE[2][1], ITEM_TABLE[2][2]);
+    
+    }
+
+    private static void printTable() {
+
         System.out.println(TABLE_TITLE);
         System.out.println(COLUMN_INDEX_POINTERS);
         System.out.println(COLUMN_SPECIFIERS);
         System.out.println(HORIZONTAL_LINE);
-        System.out.println(FIRST_ROW);
+        System.out.println(firstRow);
         System.out.println(HORIZONTAL_LINE);
-        System.out.println(SECOND_ROW);
+        System.out.println(secondRow);
         System.out.println(HORIZONTAL_LINE);
-        System.out.println(THIRD_ROW);
+        System.out.println(thirdRow);
         System.out.println(HORIZONTAL_LINE);
+
     }
 }
