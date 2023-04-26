11eb0a79f467b2e7dd8af10a89a2124d0981151b
Durim Miziraj <durim.miziraj@gmail.com>
2023-04-26 12:34:52 +0200

    Fixed problem with the datainputs in the 2d array.

--- Main.java
+++ Main.java
@@ -23,7 +23,7 @@ public class Main {
         // Declaring a 2-dimensional array.
         String[][] ITEM_TABLE = new String[][] {
             {pos00, pos01, pos02},
-            {pos01, pos11, pos12},
+            {pos10, pos11, pos12},
             {pos20, pos21, pos22}
         };
 
